66f8bbe9b6d542904304358ef1a0d35747da7f6a
shahd Alaa <shahdalaa3172003@gmail.com>
2026-03-15 13:05:09 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Q1
+// Q1: What will this print and explain what happens?
+
+// double d = 9.99;
+// int x = (int)d;
+// Console.WriteLine(x);
+
+// print 9
+
+//   When you cast a double to an int using (int), 
+//   C# performs truncation, which means it simply "chops off" the decimal part (.99) rather than rounding it. 
+//   This is an explicit cast because it results in data loss.
+    #endregoin
+    
